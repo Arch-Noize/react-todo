671675c49e497389c023d57df8bcc19cc76bd31c
Arch-Noize <santiagonoize@gmail.com>
2023-08-21 10:56:48 -0400

    Add Editing function

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,12 +1,45 @@
-const TodoItem = ({ itemProp, handleChange, delTodo }) => {
-    return <li>
-        <input 
-            type="checkbox"
-            checked={itemProp.completed}
-            onChange={() => handleChange(itemProp.id)}
+import { useState } from 'react';
+import styles from '../styles/TodoItem.module.css'
+
+const TodoItem = ({ itemProp, handleChange, delTodo, setUpdate }) => {
+    const [editing, setEditing] = useState(false);
+
+    const handleEditing = () => {
+        setEditing(true);
+    };
+    let viewMode = {};
+    let editMode = {};
+    if (editing) {
+        viewMode.display = 'none';
+    } else {
+        editMode.display = 'none';
+    }
+
+    const handleUpdatedDone = (event) => {
+        if (event.key === 'Enter') {
+          setEditing(false);
+        }
+    };
+
+    return <li className={styles.item}>
+        <div className={styles.content} style={viewMode}>
+            <input 
+                type="checkbox"
+                checked={itemProp.completed}
+                onChange={() => handleChange(itemProp.id)}
+            />
+            <button onClick={handleEditing}>Edit</button>
+            <button onClick={() => delTodo(itemProp.id)}>Delete</button>
+            {itemProp.title}
+        </div>
+        <input
+        type="text"
+        value={itemProp.title}
+        style={editMode}
+        className={styles.textInput}
+        onChange={(e) => setUpdate(e.target.value, itemProp.id)}
+        onKeyDown={handleUpdatedDone}
         />
-        <button onClick={() => delTodo(itemProp.id)}>Delete</button>
-        {itemProp.title}
         </li>;
   };
-  export default TodoItem;
+export default TodoItem;
--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,21 +1,22 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from "uuid";
 import InputTodo from './InputTodo';
 import TodosList from './TodosList';
 
 const TodosLogic = () => {
   const [todos, setTodos] = useState([
     {
-      id: 1,
+      id: uuidv4(),
       title: 'Setup development environment',
       completed: true,
     },
     {
-      id: 2,
+      id: uuidv4(),
       title: 'Develop website and add content',
       completed: false,
     },
     {
-      id: 3,
+      id: uuidv4(),
       title: 'Deploy to live server',
       completed: false,
     },
@@ -36,16 +37,42 @@ const TodosLogic = () => {
   };
 
   const delTodo = (id) => {
-    console.log('deleted', id);
+    setTodos([
+        ...todos.filter((todo) => {
+          return todo.id !== id;
+        }),
+      ]);
   };
+
+  const addTodoItem = (title) => {
+    const newTodo = {
+        id: uuidv4(),
+        title: title,
+        completed: false,
+      };
+      setTodos([...todos, newTodo]);
+  };
+
+  const setUpdate = (updatedTitle, id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.title = updatedTitle;
+        }
+        return todo;
+      })
+    );
+  };
+  
 
   return (
     <div>
-      <InputTodo />
+      <InputTodo addTodoItem={addTodoItem} />
       <TodosList 
         todosProps={todos}
         handleChange={handleChange} 
         delTodo={delTodo}
+        setUpdate={setUpdate}
         />
       
     </div>
